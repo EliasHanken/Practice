fdd895e3ffd80c239839aa06aa44225ec6157f17
Streafe <streafe@gmail.com>
2018-08-23 20:21:32 +0200

    added main repository

---
+++ Practice/src/me/streafe/practice/Practice.java
@@ -0,0 +1,18 @@
+package me.streafe.practice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Practice extends JavaPlugin{
+
+	private Practice practice;
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	public Practice getPlugin() {
+		return practice;
+	}
+	
+}
